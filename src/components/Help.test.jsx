86e9adf918cb0bed3ab4a656fbeb19fd516947ec
robertcrockett <robert.crockett@optum.com>
2024-02-16 08:16:25 -0500

    MemoizedHelp test

--- src/components/Help.test.jsx
+++ src/components/Help.test.jsx
@@ -1,10 +1,10 @@
 import { render, screen } from "@testing-library/react";
 import { describe, expect, it, vi } from "vitest";
-import Help from "./Help";
+import { MemoizedHelp } from "./Help";
 
 describe("Renders the Help component", () => {
   it("Renders the Help component", () => {
-    render(<Help />);
+    render(<MemoizedHelp />);
 
     const helpText = screen.queryByTestId("help");
     expect(helpText).toHaveTextContent(
